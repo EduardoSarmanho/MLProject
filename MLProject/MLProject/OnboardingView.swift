ef0e7705f12613553a17461198e5a90c1f0f8937
Lucas Iankowski <lucasicsilva@hotmail.com>
2020-05-05 17:41:20 -0300

    Aguardando design final

---
+++ MLProject/MLProject/OnboardingView.swift
@@ -0,0 +1,9 @@
+//
+//  OnboardingView.swift
+//  MLProject
+//
+//  Created by Lucas Iankowski Corrêa da Silva on 05/05/20.
+//  Copyright © 2020 Eduardo Sarmanho. All rights reserved.
+//
+
+import Foundation
---
+++ MLProject/MLProject/OnboardingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  OnboardingViewController.swift
+//  MLProject
+//
+//  Created by Lucas Iankowski Corrêa da Silva on 04/05/20.
+//  Copyright © 2020 Eduardo Sarmanho. All rights reserved.
+//
+
+import Foundation
